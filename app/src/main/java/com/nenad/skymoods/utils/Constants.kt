5dc70721ad466eb76ec4054350058e2c114a26ca
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-07-05 20:30:45 +0200

    hiding api key

---
+++ app/src/main/java/com/nenad/skymoods/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.nenad.skymoods.utils
+
+object Constants {
+    val base_url = "http://api.openweathermap.org/data/"
+
+}
--- app/src/main/java/com/nenad/skymoods/views/MainActivity.kt
+++ app/src/main/java/com/nenad/skymoods/views/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.nenad.skymoods.views
 
+import android.content.pm.ApplicationInfo
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.nenad.skymoods.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val ai: ApplicationInfo = applicationContext.packageManager
+            .getApplicationInfo(applicationContext.packageName, PackageManager.GET_META_DATA)
+        val value = ai.metaData["keyValue"]
+
+        val key = value.toString()
+        Toast.makeText(applicationContext,key,Toast.LENGTH_LONG).show()
     }
 }
