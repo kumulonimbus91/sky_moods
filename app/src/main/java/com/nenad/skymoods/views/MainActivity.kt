626faec3a1d656a9e67d5b3b85c382315fcdc0be
kumulonimbus91 <69019613+kumulonimbus91@users.noreply.github.com>
2022-07-05 17:51:56 +0200

    Initial commit

---
+++ app/src/main/java/com/nenad/skymoods/views/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nenad.skymoods.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nenad.skymoods.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
